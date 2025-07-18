2eeb23808d7fdef442eb8b3d7e4e9450ba8e617a
Tippztraders <tippzcashtraders@gmail.com>
2025-07-18 19:26:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const gallerySwiper = new Swiper('.gallery-swiper', {
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const videoSwiper = new Swiper('.video-swiper', {
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});

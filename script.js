d2b909116362fbb29705a893f9a92ac835351bb7
Tippztraders <tippzcashtraders@gmail.com>
2025-07-18 21:13:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 
-const gallerySwiper = new Swiper('.gallery-swiper', {
-  loop: false,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
-
-const videoSwiper = new Swiper('.video-swiper', {
-  loop: false,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
